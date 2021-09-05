63eefd4ac5b79ea0de7537579b170db7ac47256d
RR185349 <rr185349@ncr.com>
2021-09-06 00:01:45 +0530

    Entity Framework Setup

---
+++ Authentication/Context/WebApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Authentication.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Context
+{
+    public class WebApiContext : DbContext
+    {
+        public WebApiContext(DbContextOptions<WebApiContext> options) : base(options) { }
+
+        public DbSet<UserInfo> Users { get; set; }
+    }
+}
--- Authentication/Startup.cs
+++ Authentication/Startup.cs
@@ -1,14 +1,10 @@
+using Authentication.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Authentication
 {
@@ -25,6 +21,13 @@ namespace Authentication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<WebApiContext>(options =>
+                {
+                    var connectionString = Configuration.GetConnectionString("Database");
+                    options.UseSqlServer(connectionString);
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
